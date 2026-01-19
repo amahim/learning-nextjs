1b830a4a50461aa92512290792f8da1cf7fb5788
amahim <arifbin71@gmail.com>
2026-01-19 11:59:39 +0600

    Add image and description for Visit Indonesia section on the homepage

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import IndonesiaImage from "@/public/Capture4.jpg";
 const Home = () => {
   return (
     <div className="">
       <h1>Welcome to the Next.js App</h1>
+      <div>
+        <p className="text-xl text-blue-500 font-bold">Visit Indonesia</p>
+        <Image 
+        src={IndonesiaImage}
+        alt="Capture4"
+        width={500}
+        height={300}
+        blurDataURL=""
+        placeholder="blur"
+        
+        />
+      </div>
     </div>
   );
 };
